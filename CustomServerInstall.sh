d380badb39dc3a03a7d241c569b8500e41e88116
piersonjarvis <33534845+piersonjarvis@users.noreply.github.com>
2018-11-07 11:38:48 -0800

    Update and rename ServerInstall.sh.old to CustomServerInstall.sh

--- CustomServerInstall.sh
+++ CustomServerInstall.sh
@@ -5,6 +5,8 @@ curl -fsSL get.docker.com | sh
 wait
 sudo usermod -aG docker "$USER"
 sudo apt-get install docker-compose -y
+sudo mkdir /home/minecraft
+sudo chown "$USER":"$USER" /home/minecraft
 wget https://github.com/piersonjarvis/CubeCoders-AMP-Server-Docker/archive/master.zip
 unzip master.zip
 rm -r master.zip
