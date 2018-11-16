11ad8f006b2c099971794a84f8e21c2839b853c8
piersonjarvis <33534845+piersonjarvis@users.noreply.github.com>
2018-11-16 12:08:05 -0800

    Update CustomServerInstall.sh

--- CustomServerInstall.sh
+++ CustomServerInstall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt-get update && sudo apt-get upgrade -y
-sudo apt-get install unzip wget -y
+sudo apt-get install unzip wget dialog -y
 curl -fsSL get.docker.com | sh 
 wait
 sudo usermod -aG docker "$USER"
@@ -12,9 +12,12 @@ unzip master.zip
 rm -r master.zip
 mv CubeCoders-AMP-Server-Docker-master Server
 cd Server
-read -pr "Enter AMP key:" newkey;
-read -pr "Enter new AMP username:" newuser;
-read -pr "Enter new AMP password:" newpassword;
+newkey=$(dialog --stdout --title "amo key" --inputbox "Please enter amp server key:" 0 0)
+#read -pr "Enter AMP key:" newkey;
+newuser=$(dialog --stdout --title "Username" --inputbox "Please enter new username:" 0 0)
+#read -pr "Enter new AMP username:" newuser;
+password=$(dialog --stdout --title "Password" --PasswordBox "Please enter new password:" 0 0)
+#read -pr "Enter new AMP password:" newpassword;
 touch .env
 { echo "\$key=$newkey"; echo "\$password=$newpassword"; echo "\$username=$newuser"; } >> .env
 sudo docker build -q -t amp-server .
